2967ebe7c568c69922e4bb6a2d92360a12594e7c
kursor <skurochkin298@gmail.com>
2025-08-01 04:25:04 +0300

    added run method for single input/output

--- ktensorflow-core/src/commonMain/kotlin/dev/kursor/ktensorflow/Interpreter.kt
+++ ktensorflow-core/src/commonMain/kotlin/dev/kursor/ktensorflow/Interpreter.kt
@@ -6,7 +6,7 @@ package dev.kursor.ktensorflow
 interface Interpreter {
 
     /**
-     * Runs model inference.
+     * Runs model inference for multiple inputs and outputs.
      * Result of the inference will be written to the output [Tensor]s, which should be
      * allocated beforehand and passed to this method.
      * @param inputs List of input [Tensor]s.
@@ -30,3 +30,15 @@ expect fun Interpreter(
     modelDesc: ModelDesc,
     options: InterpreterOptions,
 ): Interpreter
+
+/**
+ * Runs model inference for single input and output.
+ * Result of the inference will be written to the output [Tensor], which should be
+ * allocated beforehand and passed to this method.
+ * @param input Input [Tensor].
+ * @param output Output [Tensor].
+ */
+fun Interpreter.run(
+    input: Tensor,
+    output: Tensor
+) = run(listOf(input), listOf(output))
